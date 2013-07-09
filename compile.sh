c46d64f12b014bb112a9101a88272689a5db1f6e
Daniel Kuppitz <daniel_kuppitz@hotmail.com>
2013-07-09 21:29:32 +0200

    setup github pages

--- compile.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-curl -v -X POST -d name=SQL2Gremlin -d repo=skuppitz%2Fsql2gremlin -d travis=false -d issues=false -d twitter=dkuppitz --data-urlencode content@README.md http://documentup.com/compiled > index.html
