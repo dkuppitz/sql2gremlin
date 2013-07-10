d13a37596277604171bb72d1e6a1c0697e8c4a69
Daniel Kuppitz <daniel_kuppitz@hotmail.com>
2013-07-10 11:44:12 +0200

    optimized compile script

--- compile.sh
+++ compile.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-curl -v -X POST -d name=SQL2Gremlin -d repo=skuppitz%2Fsql2gremlin -d travis=false -d issues=false -d twitter=dkuppitz --data-urlencode content@README.md http://documentup.com/compiled > index.html
+curl -v -X POST -d name=SQL2Gremlin -d repo=skuppitz%2Fsql2gremlin -d travis=false -d issues=false -d twitter=dkuppitz --data-urlencode content@README.md http://documentup.com/compiled > /tmp/index.html
+(head -n9 /tmp/index.html && tail -n+27 /tmp/index.html) > index.html
