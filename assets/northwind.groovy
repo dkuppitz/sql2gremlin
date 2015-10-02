d0961a97bac5ba70510e321dd257fd9eb7cca965
Daniel Kuppitz <daniel_kuppitz@hotmail.com>
2015-10-02 23:57:38 +0200

    minor tweaks

--- assets/northwind.groovy
+++ assets/northwind.groovy
@@ -1,14 +1,14 @@
 class NorthwindFactory {
 
   public static Graph createGraph() {
-    def graph = new TinkerGraph()
+    def graph = TinkerGraph.open()
     NorthwindFactory.load(graph, 'http://sql2gremlin.com/assets/northwind.kryo')
     return graph
   }
 
   public static void load(final Graph graph, final String path) {
-    graph.createKeyIndex('name', Vertex.class)
-    graph.createKeyIndex('customerId', Vertex.class)
-    graph.io(IoCore.kryo()).loadGraph(path)
+    graph.createIndex('name', Vertex.class)
+    graph.createIndex('customerId', Vertex.class)
+    graph.io(IoCore.gryo()).readGraph(path)
   }
 }
--- publish.sh
+++ publish.sh
@@ -2,11 +2,15 @@
 
 ./compile.sh
 
-mv index.html /tmp
+mkdir -p /tmp/sql2gremlin
+mv index.html /tmp/sql2gremlin
+cp -R assets/ /tmp/sql2gremlin
 git checkout gh-pages
 git fetch origin
 git merge origin/gh-pages
-mv /tmp/index.html ./
+mv /tmp/sql2gremlin/index.html ./
+mv /tmp/sql2gremlin/assets/* assets/
+rm -rf /tmp/sql2gremlin
 
 git commit -am 'regenerate'
 git push origin gh-pages
