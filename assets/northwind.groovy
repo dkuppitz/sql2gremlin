865ecfdbfb29a939025c8600422ea4ba26414d9b
Daniel Kuppitz <daniel_kuppitz@hotmail.com>
2015-10-02 23:44:53 +0200

    regenerate

--- assets/northwind.groovy
+++ assets/northwind.groovy
@@ -1,21 +1,14 @@
 class NorthwindFactory {
 
-  static {
-    Gremlin.load()
-  }
-
   public static Graph createGraph() {
     def graph = new TinkerGraph()
-    NorthwindFactory.load(graph, 'http://sql2gremlin.com/assets/northwind.xml')
+    NorthwindFactory.load(graph, 'http://sql2gremlin.com/assets/northwind.kryo')
     return graph
   }
 
-  public static void load(final Graph g, final String path) {
-    g.createKeyIndex('type', Vertex.class)
-    g.createKeyIndex('region', Vertex.class)
-    g.createKeyIndex('country', Vertex.class)
-    g.createKeyIndex('customerId', Vertex.class)
-    g.createKeyIndex('categoryName', Vertex.class)
-    g.loadGraphML(path)
+  public static void load(final Graph graph, final String path) {
+    graph.createKeyIndex('name', Vertex.class)
+    graph.createKeyIndex('customerId', Vertex.class)
+    graph.io(IoCore.kryo()).loadGraph(path)
   }
 }
