2d1498afcf952c58ce622fdf66274e3510fb855a
Daniel Kuppitz <daniel_kuppitz@hotmail.com>
2016-12-29 12:41:49 +0100

    made Gremlin traversals copy/paste friendly

--- compile.sh
+++ compile.sh
@@ -3,5 +3,6 @@
 preprocessor/preprocess.sh  && \
   asciidoc -b html5 -a data-uri -a icons -a toc2 -a theme=flask sql2gremlin.asciidoc.preprocessed && \
   rm sql2gremlin.asciidoc.preprocessed && \
-  sed 's/^div\.content {.*$/\0\n  overflow-x: auto;/' sql2gremlin.asciidoc.html > index.html && \
+  sed -e 's/^div\.content {.*$/\0\n  overflow-x: auto;/' \
+      -e 's/^\.\.*[1-9][0-9]*&gt;/        /' sql2gremlin.asciidoc.html > index.html && \
   rm sql2gremlin.asciidoc.html
